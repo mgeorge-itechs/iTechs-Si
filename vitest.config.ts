0d348c7a693fdb91dfe1e2842f7fe23bcc42ca75
mgeorge-itechs <m.george@itechs-group.com>
2026-09-22 13:22:26 +0300

    update vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
+const viteConfig = defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./src/test/setup.ts"],
-    include: ["src/**/*.{test,spec}.{ts,tsx}"],
-  },
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
 });
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: ["./src/test/setup.ts"],
+      include: ["src/**/*.{test,spec}.{ts,tsx}"],
+    },
+  })
+);
